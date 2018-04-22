50471215f1b2dece614bb0ef7f34cfa5e52198f1
eosiosg <eosiosg@gmail.com>
2018-04-23 00:55:21 +0800

    update api address to public testnet

--- one-click-join/2_download_configuration.sh
+++ one-click-join/2_download_configuration.sh
@@ -1,7 +1,7 @@
-wget http://testapi.eosio.sg$(cat ./path.txt)
+wget http://api.eosio.sg$(cat ./path.txt)
 
-tar xfp eosiosg.tar && rm eosiosg.tar
+tar xfp eosiosg.tar && rm eosiosg.tar ;
 
-sed -i '' -e  "s/\"\"/\"$privateKey\"]/g" eosiosg/config-dir/config.ini
+sed -i '' -e  "s/\"\"/\"$privateKey\"/g" eosiosg/config-dir/config.ini
 
 ./3_start_node.sh
