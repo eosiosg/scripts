600c81f3be718427dcb0bfd00fd7c4d8cdfcc70e
eosiosg <eosiosg@gmail.com>
2018-04-24 00:06:34 +0800

    Fix: failed to connect

--- one-click-boot/3_create_account.sh
+++ one-click-boot/3_create_account.sh
@@ -1,1 +1,1 @@
-cleos create account eosio myaccount EOS6rswKpTVQiCgMaSFbf1jioYWZJ1aTkpwxB9M9YnjZZpBoGuMMZ EOS6rswKpTVQiCgMaSFbf1jioYWZJ1aTkpwxB9M9YnjZZpBoGuMMZ
+cleos -p 8800 --wallet-port 8000 create account eosio myaccount EOS6rswKpTVQiCgMaSFbf1jioYWZJ1aTkpwxB9M9YnjZZpBoGuMMZ EOS6rswKpTVQiCgMaSFbf1jioYWZJ1aTkpwxB9M9YnjZZpBoGuMMZ
