8e47f365eb868fb4957b73f438e2f4261ed1dd3d
eosiosg <eosiosg@gmail.com>
2018-04-24 00:03:38 +0800

    Fix : modify the folder path

--- one-click-boot/2_deploy_bios.sh
+++ one-click-boot/2_deploy_bios.sh
@@ -1,5 +1,5 @@
 CLEOS=cleos
-EOS_BUILD_DIR=~/CLionProjects/eos/build
+EOS_BUILD_DIR=~/eos/build
 
 cleos -p 8800 --wallet-port 8000 wallet create
 cleos -p 8800 --wallet-port 8000 set contract eosio $EOS_BUILD_DIR/contracts/eosio.bios/ -p eosio
