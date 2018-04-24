1428e7474e19bf6434ea8af225340d30d41c9f9b
eosiosg <eosiosg@gmail.com>
2018-04-24 23:48:22 +0800

    Modify: convert to default port

--- one-click-boot/5_deploy_system.sh
+++ one-click-boot/5_deploy_system.sh
@@ -1,2 +1,2 @@
-cleos -p 8800 --wallet-port 8000  set contract eosio ~/eos/build/contracts/eosio.system/ -p eosio
-cleos  -p 8800 --wallet-port 8000 push action eosio issue '{"to":"eosio", "quantity":"1000000000.0000 EOS"}' -p eosio@active
+cleos set contract eosio ~/eos/build/contracts/eosio.system/ -p eosio
+cleos push action eosio issue '{"to":"eosio", "quantity":"1000000000.0000 EOS"}' -p eosio@active
