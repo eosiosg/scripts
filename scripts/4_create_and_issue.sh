24e6d4a7a5cdacee18708c646c4a2eaa4e29cc6d
eosiosg <eosiosg@gmail.com>
2018-05-17 22:36:55 +0800

    Update: scripts for dawn-4.0

---
+++ scripts/4_create_and_issue.sh
@@ -0,0 +1,2 @@
+cleos push action eosio.token create '[ "eosio", "1000000000.0000 EOS", 0, 0, 0]' -p eosio.token
+cleos push action eosio.token issue '["eosio", "1000000000.0000 EOS", "xxxxx"]' -p eosio
--- scripts/9_vote_producer.sh
+++ scripts/9_vote_producer.sh
@@ -1,3 +1,10 @@
-cleos push action eosio delegatebw '{"from":"voter","receiver":"voter","stake_net_quantity":"23.0000 EOS","stake_cpu_quantity":"20.0000 EOS","stake_storage_quantity":"1.0000 EOS"}' -p voter
-cleos push action eosio voteproducer '["voter", "", ["producer1"]]' -p voter
+#cleos push action eosio delegatebw '{"from":"voter","receiver":"voter","stake_net_quantity":"23.0000 EOS","stake_cpu_quantity":"20.0000 EOS","stake_storage_quantity":"1.0000 EOS"}' -p voter
+#cleos push action eosio voteproducer '["voter", "", ["producer1"]]' -p voter
 #cleos system voteproducer prods voter producer1 -p voter
+
+cleos system delegatebw frank frank "100000000.0000 EOS" "100000000.0000 EOS" --transfer
+cleos system voteproducer prods frank frank producer
+
+#cleos push action eosio voteproducer '["xiaohua", "", ["frank", "producer"]]' -p xiaohua
+
+
