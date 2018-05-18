166f9f24dda4593a21f3f1f23943dd7ee137e3ca
eosiosg <eosiosg@gmail.com>
2018-05-18 10:09:20 +0800

    update: wallet port

--- scripts/1_start.sh
+++ scripts/1_start.sh
@@ -1,5 +1,5 @@
 mkdir -p logs
 nohup nodeos --data-dir ./data-dir --config-dir ./config-dir  > ./logs/eos.log 2>&1 &
 echo $! > eos.pid
-nohup keosd -d ./data-dir --http-server-address localhost:8000 >./logs/wallet.log 2>&1 &
+nohup keosd -d ./data-dir --http-server-address localhost:8900 >./logs/wallet.log 2>&1 &
 echo $! > wallet.pid
--- scripts/2_deploy_bios.sh
+++ scripts/2_deploy_bios.sh
@@ -1,2 +1,3 @@
 cleos wallet create
+cleos wallet open
 cleos set contract eosio $EOS_BUILD_DIR/contracts/eosio.bios/ -p eosio
