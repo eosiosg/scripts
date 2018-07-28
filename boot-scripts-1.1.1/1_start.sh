2fefe7c489ba1b2e86e0ff2ab7d460420186fbdf
qianxiaofeng <mhfred.deadlock@gmail.com>
2018-07-28 11:00:20 +0800

    update boot scripts to 1.1.1

--- boot-scripts-1.1.1/1_start.sh
+++ boot-scripts-1.1.1/1_start.sh
@@ -9,4 +9,4 @@ echo "Waiting nodeos start for 3 seconds"
 sleep 3
 cleos wallet create > wallet.pw
 #cleos wallet import 5JFGWibbcKfq1ft2e8rNXFQoXUTw2qombWvCmaYVmE2e3VxtzAK
-cleos wallet import 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+cleos wallet import --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
--- boot-scripts-1.1.1/5_deploy_system.sh
+++ boot-scripts-1.1.1/5_deploy_system.sh
@@ -3,7 +3,7 @@
 
 
 cleos create account eosio b1 EOS54HgSQ9d6qjUT7pEZgbP83zQpcymR4QW1jz2jPDEdbAeKGaUif EOS54HgSQ9d6qjUT7pEZgbP83zQpcymR4QW1jz2jPDEdbAeKGaUif
-cleos wallet import 5KE3vxAZ5tBXubjMeFJ9uCHHjfQeAzDqPLeW4XHGVcuKHPPLCrA
+cleos wallet import --private-key 5KE3vxAZ5tBXubjMeFJ9uCHHjfQeAzDqPLeW4XHGVcuKHPPLCrA
 cleos transfer eosio b1 "100000000.0000 EOS" "transfer 100M to b1"
 
 cleos set contract eosio $EOS_BUILD_DIR/contracts/eosio.system -p eosio
