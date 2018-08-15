2c9e4144d0da50f0715a2f5486582a5a1228f24f
deadlock <mhfred.deadlock@gmail.com>
2018-08-15 20:08:46 +0800

    update

--- ide-debug/1_start.sh
+++ ide-debug/1_start.sh
@@ -5,6 +5,8 @@ mkdir -p "$EOS_ROOT/keosd"
 
 nohup keosd -d "$EOS_ROOT/keosd" --http-server-address localhost:8900 > "$EOS_ROOT/keosd/wallet.log" 2>&1 &
 echo $! > "$EOS_ROOT/keosd/wallet.pid"
+sleep 1;
 
 cleos wallet create > "$EOS_ROOT/keosd/wallet.pw"
 cleos wallet import --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+
